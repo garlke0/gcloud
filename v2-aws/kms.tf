3f024e6f774f639ca24c69ced441645d7f04787e
gharq <x09p77@gmail.com>
2026-09-26 01:42:57 +0800

    v2: cloudtrail with kms encryption and scoped key policy

--- v2-aws/kms.tf
+++ v2-aws/kms.tf
@@ -2,6 +2,43 @@ resource "aws_kms_key" "main" {
   description             = "gcloud v2 - customer managed key"
   deletion_window_in_days = 14
   enable_key_rotation     = true
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableIAMUserPermissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowCloudTrailEncrypt"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "kms:GenerateDataKey*"
+        Resource = "*"
+        Condition = {
+          StringLike = {
+            "kms:EncryptionContext:aws:cloudtrail:arn" = "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
+          }
+        }
+      },
+      {
+        Sid    = "AllowCloudTrailDescribe"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "kms:DescribeKey"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_kms_alias" "main" {
